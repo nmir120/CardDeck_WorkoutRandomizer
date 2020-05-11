d8023807b4915c4be2418a196ffcf521a223f99a
Numan Mir <nmir@uoguelph.ca>
2020-05-10 21:08:14 -0400

    setting up react env

---
+++ web_app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>test</h1>
+            <p>testtestestest</p>
+        </div>
+    );
+}
+
+export default App;
